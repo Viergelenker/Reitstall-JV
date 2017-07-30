a0c7c487c616c8272ecece93587ca73dace7cbb9
Julien Vollweiter <viergelenker@web.de>
2017-07-30 13:18:56 +0200

    Fixed some issues with the footer menu

--- footer.php
+++ footer.php
@@ -9,54 +9,48 @@
 ?>
 
 <footer class="footer" role="contentinfo">
+
     <div class="row">
 
-        <div class="small-12 medium-6 columns">
-            <div class="small-12 columns">
+        <div class="small-10 small-push-1 medium-6 float-left columns">
+            <form class="footer-form" action="<?php echo get_template_directory_uri(); ?>/parts/contact-email.php"
+                  method="post">
+                <div class="medium-10 columns">
+                    <label>
+                        <label for="email">Kontakt:</label>
+                        <input name="email" type="email" id="email" placeholder="Email Adresse"/>
+                    </label>
+                </div>
+                <div class="medium-10 columns">
+                    <label>
+                        <textarea rows="5" name="message" id="message" placeholder="Nachricht"></textarea>
+                    </label>
+                </div>
+                <div class="medium-10 end columns">
+                    <button name="send" class="submit" type="submit" value="Submit">Senden</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="small-10 small-push-1 medium-6 end columns">
+            <div class="small-12 columns footerMenu">
+                <p class="sitemap">Seiten:</p>
                 <nav role="navigation">
 					<?php joints_footer_links(); ?>
                 </nav>
             </div>
-
-            <ul class="inline-list social">
-
-            </ul>
-            <p class="copywrite">Bilder und Text sind Eigentum von Reitstall Räuchle.</p>
-            <p class="copywrite">Es wird keine Haftung für Aussagen oder Verlinkungen übernommen.</p>
-        </div>
-
-        <div class="small-12 medium-6 columns">
-            <p class="logo show-for-small-only"><i class="fi-target"></i> Reitstall Räuchle</p>
-            <form class="footer-form" action="<?php echo get_template_directory_uri(); ?>/parts/contact-email.php"
-                  method="post">
-                <div class="row">
-                    <div class="medium-9 medium-push-3 columns">
-                        <label>
-                            <label for="email">Kontakt:</label>
-                            <input name="email" type="email" id="email" placeholder="Email Adresse"/>
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="medium-9 medium-push-3 columns">
-                        <label>
-                            <textarea rows="5" name="message" id="message" placeholder="Nachricht"></textarea>
-                        </label>
-                    </div>
-                    <div class="row">
-                        <div class="medium-9 medium-push-3 columns">
-                            <button name="send" class="submit" type="submit" value="Submit">Senden</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
+            <hr/>
+            <div class="small-12 small-centered columns">
+                <p class="disclaimer">Bilder und Text sind Eigentum von Reitstall Räuchle.</p>
+                <p class="disclaimer">Es wird keine Haftung für Aussagen oder Verlinkungen übernommen.</p>
+            </div>
         </div>
 
     </div>
 
     <div id="inner-footer" class="row">
 
-        <div class="small-12 medium-12 large-12 columns">
+        <div class="small-12 text-center columns">
             <p class="source-org copyright">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>.</p>
         </div>
 
--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ require_once(get_template_directory().'/functions/enqueue-scripts.php');
 require_once(get_template_directory().'/functions/menu.php'); 
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/functions/sidebar.php'); 
+require_once(get_template_directory().'/functions/sidebar.php');
 
 // Makes WordPress comments suck less
 require_once(get_template_directory().'/functions/comments.php'); 
@@ -29,7 +29,7 @@ require_once(get_template_directory().'/functions/page-navi.php');
 require_once(get_template_directory().'/functions/translation/translation.php'); 
 
 // Adds site styles to the WordPress editor
-// require_once(get_template_directory().'/functions/editor-styles.php'); 
+require_once(get_template_directory().'/functions/editor-styles.php');
 
 // Remove 4.2 Emoji Support
 // require_once(get_template_directory().'/functions/disable-emoji.php'); 
@@ -41,7 +41,7 @@ require_once(get_template_directory().'/functions/translation/translation.php');
 // require_once(get_template_directory().'/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/functions/login.php'); 
+require_once(get_template_directory().'/functions/login.php');
 
 // Customize the WordPress admin
 // require_once(get_template_directory().'/functions/admin.php'); 
